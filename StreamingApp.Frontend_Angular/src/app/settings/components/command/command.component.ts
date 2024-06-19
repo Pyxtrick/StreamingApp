3df017add847a8c3aa8cedaa5e3e91b941ea555d
Pyxtrick <pyxtrick@gmail.com>
2024-06-19 21:06:44 +0200

    add Table with logic to request data wip

--- StreamingApp.Frontend_Angular/src/app/settings/components/command/command.component.ts
+++ StreamingApp.Frontend_Angular/src/app/settings/components/command/command.component.ts
@@ -1,23 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { CommandAndResponseDto } from 'src/api/api.service';
 import { SettingsActions } from '../../state/action';
-import { settingsFeature } from '../../state/reducers';
+import { settingsFeature } from './../../state/reducers';
 
 @Component({
   selector: 'app-command',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './command.component.html',
   styleUrl: './command.component.scss',
 })
 export class CommandComponent {
   private store = inject(Store);
-  private commands$!: Observable<CommandAndResponseDto[]>;
+  public commands$!: Observable<CommandAndResponseDto[]>;
+  public commands!: CommandAndResponseDto[];
+  displayedColumns: string[] = [
+    'command',
+    'response',
+    'description',
+    'active',
+    'auth',
+    'category',
+  ];
 
   ngOnInit(): void {
     this.store.dispatch(SettingsActions.loadCommands());
     this.commands$ = this.store.select(settingsFeature.selectCommands);
+
+    this.store
+      .select(settingsFeature.selectCommands)
+      .pipe(
+        map((command) => {
+          this.commands = command;
+        })
+      )
+      .subscribe();
+
+    console.log(this.commands);
   }
 }
