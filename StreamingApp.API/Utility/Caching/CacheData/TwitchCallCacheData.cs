fcc22c0a195b8e32887ab4209cabdaebd5e0ed38
Pyxtrick <pyxtrick@gmail.com>
2025-02-20 13:41:23 +0100

    add TODO's and sort them

--- StreamingApp.API/Utility/Caching/CacheData/TwitchCallCacheData.cs
+++ StreamingApp.API/Utility/Caching/CacheData/TwitchCallCacheData.cs
@@ -5,20 +5,23 @@ namespace StreamingApp.API.Utility.Caching.CacheData;
 public class TwitchCallCacheData
 {
     public List<MessageDto> CachedMessageData { get; set; } = new List<MessageDto>();
-
+    //Maybe Combine with Message
     public List<CommandDto> CachedCommandData { get; set; } = new List<CommandDto>();
 
+    //Combine
     public List<SubDto> CachedGiftedSubData { get; set; } = new List<SubDto>();
-
+    //Combine
     public List<SubDto> CachedNewSubData { get; set; } = new List<SubDto>();
-
+    //Combine
     public List<SubDto> CachedPrimeSubData { get; set; } = new List<SubDto>();
-
+    //Combine
     public List<SubDto> CachedReSubData { get; set; } = new List<SubDto>();
 
     public List<RaidDto> CachedRaidData { get; set; } = new List<RaidDto>();
 
+    // TODO: Change to Folow
     public List<JoinDto> CachedUserJoinData { get; set; } = new List<JoinDto>();
+    //public List<FolowDto> CachedUserFolowData { get; set; } = new List<FolowDto>();
     //public ObservableCollection<JoinDto> CachedUserJoinData { get; set; }
 
     // problebly do not need it
