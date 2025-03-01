0c6c06059d2f6b2a3b11a8759f593c09aeebbeb3
Pyxtrick <pyxtrick@gmail.com>
2025-03-01 15:37:24 +0100

    Fix await for Staring NSwag

--- StreamingApp/Program.cs
+++ StreamingApp/Program.cs
@@ -74,7 +74,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     scope.ServiceProvider.GetRequiredService<IStartTwitchApi>().Execute();
-    await scope.ServiceProvider.GetRequiredService<IEmotesApiRequest>().GetTVEmoteSet();
+    Task task = scope.ServiceProvider.GetRequiredService<IEmotesApiRequest>().GetTVEmoteSet();
 }
 
 app.Run();
