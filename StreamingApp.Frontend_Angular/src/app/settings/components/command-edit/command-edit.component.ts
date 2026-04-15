eb3b2c773a5d87c3ee270e6956f2f6236c09b729
Pyxtrick <pyxtrick@gmail.com>
2026-04-15 11:33:53 +0200

    Command-edit
    	add List Select
    	update Design

--- StreamingApp.Frontend_Angular/src/app/settings/components/command-edit/command-edit.component.ts
+++ StreamingApp.Frontend_Angular/src/app/settings/components/command-edit/command-edit.component.ts
@@ -2,10 +2,15 @@ import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { FormBuilder, FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { AuthEnum, CommandAndResponseDto } from 'src/api/api.service';
+import {
+  AuthEnum,
+  CategoryEnum,
+  CommandAndResponseDto,
+} from 'src/api/api.service';
 
 @Component({
   selector: 'app-command-edit',
@@ -15,13 +20,18 @@ import { AuthEnum, CommandAndResponseDto } from 'src/api/api.service';
     FormsModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatGridListModule,
   ],
   templateUrl: './command-edit.component.html',
   styleUrl: './command-edit.component.scss',
 })
 export class CommandEditComponent {
   command!: CommandAndResponseDto;
-  auths = AuthEnum;
+  authList = Object.keys(AuthEnum).filter((x) => isNaN(parseInt(x)));
+  authEnum = AuthEnum;
+
+  categoryList = Object.keys(CategoryEnum).filter((x) => isNaN(parseInt(x)));
+  categoryEnum = CategoryEnum;
 
   constructor(
     private fb: FormBuilder,
